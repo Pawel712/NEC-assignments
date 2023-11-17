681fc51962fbd9bf4bd134612da14901b77585fb
pavo <pawelpuzdrowski@hotmail.com>
2023-11-17 18:22:40 +0100

    Changes to file to work as jupyter file

--- Assignment1/Part3/part3_2MultiLinearRegression.py
+++ Assignment1/Part3/part3_2MultiLinearRegression.py
@@ -29,67 +29,89 @@ def train_multilinear_regression(dataset, input_columns, output_column):
 
 
 # Load the dataset
-#dataset1 = pd.read_csv('SyntheticNormalized.csv')
-#dataset2 = pd.read_csv('TurbineNormalized.csv')
+dataset1 = pd.read_csv('SyntheticNormalized.csv')
+dataset2 = pd.read_csv('TurbineNormalized.csv')
 dataset3 = pd.read_csv('RealEstateNormalizeds.csv')
 
 # Define input and output columns for dataset 2
-#input_columns1 = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9']
-#output_column1 = 'z'
+input_columns1 = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9']
+output_column1 = 'z'
 
-#input_columns2 = ['height_over_sea_level', 'fall', 'net', 'fall_1', 'flow']
-#output_column2 = 'power_of_hydroelectrical_turbine'
+input_columns2 = ['height_over_sea_level', 'fall', 'net', 'fall_1', 'flow']
+output_column2 = 'power_of_hydroelectrical_turbine'
 
 input_columns3 = ['X1 transaction date', 'X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores', 'X5 latitude', 'X6 longitude']
 output_column3 = 'Y house price of unit area'
 
-# Neural network configurations
-num_layers = 7
-#num_units1 = [len(input_columns1)] + [10, 5, 1]
-#num_units2 = [len(input_columns2)] + [10, 5, 1]
-#num_units3 = [len(input_columns3)] + [8, 4, 1]
-num_epochs = 1000
-learning_rate = 0.1
-momentum = 0.0
-activation_function = "sigmoid"
-validation_percentage = 0.2
-
 # Training the multilinear regression model for dataset 1
-#multi_reg_model, multi_reg_predictions = train_multilinear_regression(dataset1, input_columns1, output_column1)
+multi_reg_model, multi_reg_predictions1 = train_multilinear_regression(dataset1, input_columns1, output_column1)
 
 # Training the multilinear regression model for dataset 2
-#multi_reg_model, multi_reg_predictions = train_multilinear_regression(dataset2, input_columns2, output_column2)
+multi_reg_model, multi_reg_predictions2 = train_multilinear_regression(dataset2, input_columns2, output_column2)
 
 # Training the multilinear regression model for dataset 3
-multi_reg_model, multi_reg_predictions = train_multilinear_regression(dataset3, input_columns3, output_column3)
+multi_reg_model, multi_reg_predictions3 = train_multilinear_regression(dataset3, input_columns3, output_column3)
 
-
-
-# Assuming predictions2 contains predicted values and dataset1 contains the real values
-#real_values = dataset1[output_column1].values.reshape(-1, 1)
+# Assuming predictions1 contains predicted values and dataset1 contains the real values
+real_values1 = dataset1[output_column1].values.reshape(-1, 1)
 
 # Assuming predictions2 contains predicted values and dataset1 contains the real values
-#real_values = dataset2[output_column2].values.reshape(-1, 1)
+real_values2 = dataset2[output_column2].values.reshape(-1, 1)
 
-# Assuming predictions2 contains predicted values and dataset1 contains the real values
-real_values = dataset3[output_column3].values.reshape(-1, 1)
+# Assuming predictions3 contains predicted values and dataset1 contains the real values
+real_values3 = dataset3[output_column3].values.reshape(-1, 1)
 
 # Calculate MAPE for dataset 2
-mape2 = calculate_mape(real_values, multi_reg_predictions)
+mape1 = calculate_mape(real_values1, multi_reg_predictions1)
+print(f"MAPE for Dataset Synthetic: {mape1:.2f}%")
+
+# Calculate MAPE for dataset 2
+mape2 = calculate_mape(real_values2, multi_reg_predictions2)
 print(f"MAPE for Dataset turbine: {mape2:.2f}%")
+
+# Calculate MAPE for dataset 2
+mape3 = calculate_mape(real_values3, multi_reg_predictions3)
+print(f"MAPE for Dataset RealEstate: {mape3:.2f}%")
+
+# Visualize the scatter plot for dataset 1
+plt.figure(figsize=(8, 6))
+plt.scatter(real_values1, multi_reg_predictions1, alpha=0.5)
+plt.xlabel('Real Values')
+plt.ylabel('Predicted Values')
+plt.title('Correlation between Real and Predicted Values (Dataset Synthetic, multi linear regression)')
+plt.grid(True)
+plt.plot([real_values1.min(), real_values1.max()], [real_values1.min(), real_values1.max()], 'k--', lw=2) # diagonal line
+plt.savefig('correlation_plot_Synthetic_multi_linear.png')
+plt.show()
+
+#Multilinear regression output:
+np.savetxt("Synthetic_predictionMultiLinearRegression.csv", multi_reg_predictions1, delimiter=",")
 
 # Visualize the scatter plot for dataset 2
 plt.figure(figsize=(8, 6))
-plt.scatter(real_values, multi_reg_predictions, alpha=0.5)
+plt.scatter(real_values2, multi_reg_predictions2, alpha=0.5)
 plt.xlabel('Real Values')
 plt.ylabel('Predicted Values')
-plt.title('Correlation between Real and Predicted Values (Dataset RealEstate, multi linear regression)')
+plt.title('Correlation between Real and Predicted Values (Dataset Turbine, multi linear regression)')
 plt.grid(True)
-plt.plot([real_values.min(), real_values.max()], [real_values.min(), real_values.max()], 'k--', lw=2) # diagonal line
+plt.plot([real_values2.min(), real_values2.max()], [real_values2.min(), real_values2.max()], 'k--', lw=2) # diagonal line
+plt.savefig('correlation_plot_Turbine_multi_linear.png')
+plt.show()
+
+#Multilinear regression output:
+np.savetxt("Turbine_predictionMultiLinearRegression.csv", multi_reg_predictions2, delimiter=",")
+
+# Visualize the scatter plot for dataset 3
+plt.figure(figsize=(8, 6))
+plt.scatter(real_values3, multi_reg_predictions3, alpha=0.5)
+plt.xlabel('Real Values')
+plt.ylabel('Predicted Values')
+plt.title('Correlation between Real and Predicted Values (Dataset Synthetic, multi linear regression)')
+plt.grid(True)
+plt.plot([real_values3.min(), real_values3.max()], [real_values3.min(), real_values3.max()], 'k--', lw=2) # diagonal line
 plt.savefig('correlation_plot_RealEstate_multi_linear.png')
 plt.show()
 
 #Multilinear regression output:
-np.savetxt("RealEstate_predictionMultiLinearRegression.csv", multi_reg_predictions, delimiter=",")
-
+np.savetxt("RealEstate_predictionMultiLinearRegression.csv", multi_reg_predictions3, delimiter=",")
 
--- Assignment1/Part3/part3_2Tensorflow.py
+++ Assignment1/Part3/part3_2Tensorflow.py
@@ -40,25 +40,26 @@ def train_neural_network_tensorflow(dataset, input_columns, output_column, num_l
 
 
 # Load the dataset
-#dataset1 = pd.read_csv('SyntheticNormalized.csv')
-#dataset2 = pd.read_csv('TurbineNormalized.csv')
+dataset1 = pd.read_csv('SyntheticNormalized.csv')
+dataset2 = pd.read_csv('TurbineNormalized.csv')
 dataset3 = pd.read_csv('RealEstateNormalizeds.csv')
 
 
 # Define input and output columns for dataset 2
-#input_columns1 = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9']
-#output_column1 = 'z'
+input_columns1 = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9']
+output_column1 = 'z'
 
-#input_columns2 = ['height_over_sea_level', 'fall', 'net', 'fall_1', 'flow']
-#output_column2 = 'power_of_hydroelectrical_turbine'
+input_columns2 = ['height_over_sea_level', 'fall', 'net', 'fall_1', 'flow']
+output_column2 = 'power_of_hydroelectrical_turbine'
 
 input_columns3 = ['X1 transaction date', 'X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores', 'X5 latitude', 'X6 longitude']
 output_column3 = 'Y house price of unit area'
 
 # Neural network configurations
 num_layers = 4
-#num_units1 = [len(input_columns1)] + [10, 5, 1]
-#num_units2 = [len(input_columns2)] + [10, 5, 1]
+numLayersSynthetic = 7
+num_units1 = [len(input_columns1)] + [10, 5, 1]
+num_units2 = [len(input_columns2)] + [10, 5, 1]
 num_units3 = [len(input_columns3)] + [8, 4, 1]
 num_epochs = 1000
 learning_rate = 0.1
@@ -67,45 +68,77 @@ activation_function = "sigmoid"
 validation_percentage = 0.2
 
 # Training the neural network using TensorFlow for dataset 1
-#tf_nn_model, tf_nn_predictions, tf_nn_history = train_neural_network_tensorflow(dataset1, input_columns1, output_column1, num_layers, num_units1, num_epochs, learning_rate)
+tf_nn_model1, tf_nn_predictions1, tf_nn_history1 = train_neural_network_tensorflow(dataset1, input_columns1, output_column1, numLayersSynthetic, num_units1, num_epochs, learning_rate)
 
 # Training the neural network using TensorFlow for dataset 2
-#tf_nn_model, tf_nn_predictions, tf_nn_history = train_neural_network_tensorflow(dataset2, input_columns2, output_column2, num_layers, num_units2, num_epochs, learning_rate)
+tf_nn_model2, tf_nn_predictions2, tf_nn_history2 = train_neural_network_tensorflow(dataset2, input_columns2, output_column2, num_layers, num_units2, num_epochs, learning_rate)
 
 # Training the neural network using TensorFlow for dataset 3
-tf_nn_model, tf_nn_predictions, tf_nn_history = train_neural_network_tensorflow(dataset3, input_columns3, output_column3, num_layers, num_units3, num_epochs, learning_rate)
-
-
+tf_nn_model3, tf_nn_predictions3, tf_nn_history3 = train_neural_network_tensorflow(dataset3, input_columns3, output_column3, num_layers, num_units3, num_epochs, learning_rate)
 
 # Assuming predictions2 contains predicted values and dataset2 contains the real values
-#real_values = dataset1[output_column1].values.reshape(-1, 1)
+real_values1 = dataset1[output_column1].values.reshape(-1, 1)
 
 # Assuming predictions2 contains predicted values and dataset2 contains the real values
-#real_values = dataset2[output_column2].values.reshape(-1, 1)
+real_values2 = dataset2[output_column2].values.reshape(-1, 1)
 
 # Assuming predictions2 contains predicted values and dataset2 contains the real values
-real_values = dataset3[output_column3].values.reshape(-1, 1)
+real_values3 = dataset3[output_column3].values.reshape(-1, 1)
 
-
+# Calculate MAPE for dataset 1
+mape1 = calculate_mape(real_values1, tf_nn_predictions1)
+print(f"MAPE for Dataset Synthetic: {mape1:.2f}%")
 
 # Calculate MAPE for dataset 2
-mape1 = calculate_mape(real_values, tf_nn_predictions)
-print(f"MAPE for Dataset 2: {mape1:.2f}%")
+mape2 = calculate_mape(real_values2, tf_nn_predictions2)
+print(f"MAPE for Dataset Turbine: {mape2:.2f}%")
+
+# Calculate MAPE for dataset 3
+mape3 = calculate_mape(real_values3, tf_nn_predictions3)
+print(f"MAPE for Dataset RealEstate: {mape3:.2f}%")
+
+# Visualize the scatter plot for dataset 1
+plt.figure(figsize=(8, 6))
+plt.scatter(real_values1, tf_nn_predictions1, alpha=0.5)
+plt.xlabel('Real Values')
+plt.ylabel('Predicted Values')
+plt.title('Correlation between Real and Predicted Values (Dataset Synthetic, tensorflow)')
+plt.grid(True)
+plt.plot([real_values1.min(), real_values1.max()], [real_values1.min(), real_values1.max()], 'k--', lw=2) # diagonal line
+plt.savefig('correlation_plot_Synthetic_tensorflow.png')
+plt.show()
+
+np.savetxt("SyntheticPredictionTensorflow.csv", tf_nn_predictions1, delimiter=",")
+np.savetxt("SyntheticLossTensorflow.csv", tf_nn_history1.history['loss'], delimiter=",")
+
 
 # Visualize the scatter plot for dataset 2
 plt.figure(figsize=(8, 6))
-plt.scatter(real_values, tf_nn_predictions, alpha=0.5)
+plt.scatter(real_values2, tf_nn_predictions2, alpha=0.5)
+plt.xlabel('Real Values')
+plt.ylabel('Predicted Values')
+plt.title('Correlation between Real and Predicted Values (Dataset Turbine, tensorflow)')
+plt.grid(True)
+plt.plot([real_values2.min(), real_values2.max()], [real_values2.min(), real_values2.max()], 'k--', lw=2) # diagonal line
+plt.savefig('correlation_plot_Turbine_tensorflow.png')
+plt.show()
+
+np.savetxt("TurbinePredictionTensorflow.csv", tf_nn_predictions2, delimiter=",")
+np.savetxt("TurbineLossTensorflow.csv", tf_nn_history2.history['loss'], delimiter=",")
+
+
+# Visualize the scatter plot for dataset 3
+plt.figure(figsize=(8, 6))
+plt.scatter(real_values3, tf_nn_predictions3, alpha=0.5)
 plt.xlabel('Real Values')
 plt.ylabel('Predicted Values')
 plt.title('Correlation between Real and Predicted Values (Dataset RealEstate, tensorflow)')
 plt.grid(True)
-plt.plot([real_values.min(), real_values.max()], [real_values.min(), real_values.max()], 'k--', lw=2) # diagonal line
+plt.plot([real_values3.min(), real_values3.max()], [real_values3.min(), real_values3.max()], 'k--', lw=2) # diagonal line
 plt.savefig('correlation_plot_RealEstate_tensorflow.png')
 plt.show()
 
-
-#Tensorflow output:
-np.savetxt("RealEstatePredictionTensorflow.csv", tf_nn_predictions, delimiter=",")
-np.savetxt("RealEstateLossTensorflow.csv", tf_nn_history.history['loss'], delimiter=",")
+np.savetxt("RealEstatePredictionTensorflow.csv", tf_nn_predictions3, delimiter=",")
+np.savetxt("RealEstateLossTensorflow.csv", tf_nn_history3.history['loss'], delimiter=",")
 
 
